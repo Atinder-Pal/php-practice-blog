d98bf510867d2c46f70cc9d9df18145f013f1c8a
Atinder-Pal <atty.pal46@gmail.com>
2020-10-02 19:16:05 -0600

    added citation

--- includes/RetrieveJsonFile.Class.php
+++ includes/RetrieveJsonFile.Class.php
@@ -4,7 +4,10 @@ class RetrieveJsonFile
     public $articlesArray;
     function __construct ( $pathToFile='' )
     {
+        //Citation
+        // Tammy helped with this code to call the method inside the constructor
         $this->articlesArray = $this->retrieveDataFromJson( $pathToFile );
+        // End Citation
     }
 
     public function retrieveDataFromJson( $pathToFile )
